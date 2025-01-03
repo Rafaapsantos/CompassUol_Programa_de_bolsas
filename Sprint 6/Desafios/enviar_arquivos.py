c4b1f921291a8b4878ee89d7e04397e32cabffb1
rafaela santos <rafaelasantosdeveloper@gmail.com>
2025-01-03 14:07:44 -0300

    (03/01) Fazendo o desafio

--- Sprint 6/Desafios/enviar_arquivos.py
+++ Sprint 6/Desafios/enviar_arquivos.py
@@ -15,28 +15,34 @@ s3_client = boto3.client(
     aws_session_token=aws_session_token
 )
 
-# Captura a data atual para o caminho S3
+# Pega a data atual no formato YYYY/MM/DD para criar os caminhos no bucket S3
 data_atual = datetime.now().strftime('%Y/%m/%d')
 
-# Caminhos locais e S3
+# Nome do bucket onde os arquivos serão armazenados
 nome_bucket = 'datalake-rafaela-santos'
 
+# Define os caminhos locais para os arquivos que vão ser enviados
 arquivos_locais = {
-    "movies.csv": "./dados/movies.csv",
-    "series.csv": "./dados/series.csv"
+    "movies.csv": "./dados/movies.csv", # Caminho do arquivo movies.csv no diretório local
+    "series.csv": "./dados/series.csv" # Caminho do arquivo series.csv no diretório local
 }
 
+# Define os caminhos de destino no bucket 
 caminhos_s3 = {
-    "movies.csv": f"Raw/Local/CSV/Movies/{data_atual}/movies.csv",
-    "series.csv": f"Raw/Local/CSV/Series/{data_atual}/series.csv"
+    "movies.csv": f"Raw/Local/CSV/Movies/{data_atual}/movies.csv", # Caminho para movies.csv no bucket
+    "series.csv": f"Raw/Local/CSV/Series/{data_atual}/series.csv" # Caminho para series.csv no bucket 
 }
-# Faz o upload dos arquivos diretamente
+
+# Itera sobre os arquivos definidos nos caminhos locais
 for nome_arquivo, caminho in arquivos_locais.items():
     if os.path.isfile(caminho):  # Verifica se o arquivo existe
         try:
+            # Faz o upload do arquivo para o bucket
             s3_client.upload_file(caminho, nome_bucket, caminhos_s3[nome_arquivo])
             print(f" O arquivo {nome_arquivo} foi enviado com sucesso para {caminhos_s3[nome_arquivo]}")
         except Exception as e:
+            # Exibe erros que possam ocorrer durante o upload
             print(f"Erro ao enviar {nome_arquivo}: {e}")
     else:
+        # Exibe uma mensagem caso o arquivo não seja encontrado no caminho local
         print(f" O arquivo {nome_arquivo} não foi encontrado em {caminho}")
