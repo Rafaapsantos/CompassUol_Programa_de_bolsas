8c107c4439e9daa408e7a11a6dd6d36ac4f4177f
Rafaapsantos <rafaelasantosdeveloper@gmail.com>
2025-01-22 19:47:33 -0300

    (22/01) Fazendo o desafio e documentando com as evidencias

--- Sprint 8/Desafios/job_csv.py
+++ Sprint 8/Desafios/job_csv.py
@@ -2,8 +2,8 @@ import sys
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from datetime import datetime
 from pyspark.sql.functions import col
+from awsglue.utils import getResolvedOptions
 
 
 ## @params: [JOB_NAME]
@@ -11,7 +11,7 @@ args = getResolvedOptions(sys.argv, ['JOB_NAME', 'S3_INPUT_PATH', 'S3_TARGET_PAT
 
 # Inicializa o SparkContext e GlueContext para manipulação dos dados
 sc = SparkContext()
-glueContext = GlueContext(SparkContext.getOrCreate())
+glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 # Inicializa o job do AWS Glue
 job = Job(glueContext)
@@ -21,13 +21,22 @@ job.init(args['JOB_NAME'], args)
 source_file = args['S3_INPUT_PATH']
 target_path = args['S3_TARGET_PATH']
 
-# Lê o arquivo CSV localizado no S3
-df = spark.read.csv(source_file, header=True, inferSchema=True, sep="|")
+# Lê o arquivo CSV localizado no S3 como DynamicFrame
+df_dynamic = glueContext.create_dynamic_frame.from_options(
+    connection_type="s3",
+    connection_options={"paths": [source_file]},
+    format="csv",
+    format_options={"withHeader": True, "separator": "|"}
+)
+
+# Converte o DynamicFrame para DataFrame
+df = df_dynamic.toDF()
 
 # Filtra os dados para selecionar apenas as linhas do gênero "Drama,Romance" e do ano de lançamento 2016 que é o ano em que teve mais lançamentos de filmes de "Drama,Romance"
 filtra_dados = df.filter(
-    (df["genero"] == "Drama,Romance") & (df["anoLancamento"] == 2016)
+    (col("genero") == "Drama,Romance") & (col("anoLancamento") == 2016)
 )
+
 # Lista de IDs IMDB de filmes que devem ser excluídos, pois não exixtem esses filmes na API do TMDB
 ids_excluir = [
     "tt11656988", "tt13231658", "tt3526416", "tt4332342", "tt5181502", 
@@ -42,15 +51,11 @@ dados_filtrados = filtra_dados.filter(~col("id").isin(ids_excluir))
 colunas_selecionadas = [
     "tituloPincipal", "tituloOriginal", "notaMedia", "numeroVotos",
     "personagem", "nomeArtista", "profissao", "anoNascimento",
-    "anoFalecimento", "anoLancamento", "tempoMinutos"
+    "anoFalecimento", "anoLancamento", "tempoMinutos", "id"
 ]
 # Seleciona apenas as colunas relevantes
-dados_limpos = dados_filtrados.select(colunas_selecionadas)
+dados_limpos = dados_filtrados.select(*colunas_selecionadas)
 
-# Pega a data atual no formato "YYYY/MM/DD" para ser usada como parte do caminho de saída no S3
-data_atual = datetime.now().strftime("%Y/%m/%d")
-
-# Escreve o DataFrame final no formato Parquet no caminho de saída, com particionamento baseado na data atual
-dados_limpos.write.mode("overwrite").parquet(target_path + "/" + data_atual)
-
+dados_limpos.write.mode("overwrite").parquet(target_path)
+ 
 job.commit()
