310de687409a391a80ec865f48bea5274f4c0d31
Rafaapsantos <rafaelasantosdeveloper@gmail.com>
2025-01-23 19:24:32 -0300

    (23/01) Fazendo o desafio e documentando com as evidencias

--- Sprint 8/Desafios/job_json.py
+++ Sprint 8/Desafios/job_json.py
@@ -5,7 +5,7 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-# Parâmetros da Glue Job
+# Inicializa o contexto do Glue
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -17,11 +17,11 @@ job.init(args['JOB_NAME'], args)
 input_path = "s3://datalake-rafaela-santos/Raw/TMDB/JSON/2025/01/17/"
 output_path = "s3://datalake-rafaela-santos/Trusted/TMDB/Parquet/movies/2025/01/17/"
 
-# Leitura do arquivo JSON
+# Lê os arquivos JSON
 datasource = spark.read.json(input_path)
 
-# Conversão para formato Parquet
+# Salva os dados em formato Parquet no destino especificado
 datasource.write.mode("overwrite").parquet(output_path)
 
-# Finalização do job
+# Finaliza o job
 job.commit()
